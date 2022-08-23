e4c63777e06291f2dda794075eedd1260d64a09f
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-08-23 13:08:24 -0500

    connect to database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/marcos-nsantos/comments-rest-api/internal/database"
+)
 
 // Run - is going to be responsible for
 // the instantiation and startup of our
 // go application.
 func Run() error {
+	db, err := database.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+	if err = db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("successfully connected and pinged the database")
+
 	fmt.Println("starting up our application")
 	return nil
 }
---
+++ internal/database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	Client *sqlx.DB
+}
+
+func NewDatabase() (*Database, error) {
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_TABLE"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("SSL_MODE"),
+	)
+
+	dbConn, err := sqlx.Connect("postgres", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to the database: %w", err)
+	}
+
+	return &Database{Client: dbConn}, nil
+}
+
+func (d *Database) Ping(ctx context.Context) error {
+	return d.Client.DB.PingContext(ctx)
+}
