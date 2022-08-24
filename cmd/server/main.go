f271fa966af1f28fc91cb5277691b2fea8369590
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-08-24 10:17:31 -0500

    setup handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,24 +3,33 @@ package main
 import (
 	"fmt"
 
+	"github.com/marcos-nsantos/comments-rest-api/internal/comment"
 	"github.com/marcos-nsantos/comments-rest-api/internal/database"
+	transportHttp "github.com/marcos-nsantos/comments-rest-api/internal/transport/http"
 )
 
 // Run - is going to be responsible for
 // the instantiation and startup of our
 // go application.
 func Run() error {
+	fmt.Println("starting up our application")
+
 	db, err := database.NewDatabase()
 	if err != nil {
 		fmt.Println("Failed to connect to the database")
 		return err
 	}
-	if err := db.MigrateDB(); err != nil {
+	if err = db.MigrateDB(); err != nil {
 		fmt.Println("failed to migrate database")
 		return err
 	}
 
-	fmt.Println("starting up our application")
+	cmtService := comment.NewService(db)
+	httpHandler := transportHttp.NewHandler(cmtService)
+	if err = httpHandler.Serve(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
